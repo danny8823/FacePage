ead282eb570a8f9b5ca05f00299620433bdac133
danny8823 <dannyyoo714@gmail.com>
2024-09-17 11:05:47 -0700

    Added login,profile,changepass,updateprofile routers

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -52,6 +52,61 @@ const usersController = {
         },process.env.JWT_SECRET,{
             expiresIn: "30d"
         })
+
+        res.json({
+            message: "Login success",
+            token
+        })
+    }),
+    profile: asyncHandler(async(req,res) => {
+        const user = await User.findById(req.user)
+
+        if(!user) {
+            throw new Error('User not found')
+        }
+
+        res.json({
+            username: user.username,
+            email: user.email
+        })
+    }),
+    changePassword: asyncHandler(async (req,res) => {
+        const {newPassword} = req.body;
+
+        const user = await User.findById('')
+
+        if(!user) {
+            throw new Error('User not found')
+        }
+
+        const salt = bcrypt.genSalt(10)
+        const hashedPass = await bcrypt.hash(newPassword, salt)
+
+        user.password = hashedPass
+
+        await user.save({
+            validateBeforeSave: false,
+        })
+
+        res.json({
+            message: 'Password change success'
+        })
+    }),
+    updateProfile: asyncHandler(async (req,res) => {
+        const {email,username} = req.body;
+        const updatedUser = await User.findByIdAndUpdate(
+            req.user,
+            {
+                username,
+                email
+            },
+            {
+                new: true
+            }
+        )
+        res.json({
+            message: 'Profile update success'
+        })
     })
 }
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,5 +3,8 @@ const usersController = require('../controllers/usersCtrl')
 const userRouter = express.Router()
 
 userRouter.post('/api/v1/users/register', usersController.register)
-
+userRouter.post('/api/v1/users/login', usersController.login)
+userRouter.get('/api/v1/users/profile',usersController.profile)
+userRouter.put('/api/v1/users/change-password', usersController.changePassword)
+userRouter.put('/api/v1/users/update-profile', usersController.updateProfile)
 module.exports = userRouter
